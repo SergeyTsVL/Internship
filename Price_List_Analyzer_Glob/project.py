9bea45d733a24d28eaefb83f79feec68a512e24c
SergeyTsVL <159886384+SergeyTsVL@users.noreply.github.com>
2024-09-21 18:04:27 +0700

    Add files via upload

--- Price_List_Analyzer_Glob/project.py
+++ Price_List_Analyzer_Glob/project.py
@@ -5,8 +5,8 @@ from airium import Airium
 file_path = '../Price_List_Analyzer_Glob/Files_for_analysis'
 file_html = '../Price_List_Analyzer_Glob/Files_for_analysis/output.html'
 list_result = []
-class PriceMachine():
-
+search = input(f"Найти: ")
+class PriceMachine:
     def load_prices(self):
         '''
             Сканирует указанный каталог. Ищет файлы со словом price в названии.
@@ -33,7 +33,6 @@ class PriceMachine():
             if "price" in file:
                 with open(f'{file_path}/{file}', mode="r", encoding='utf-8') as sort_file:
                     for row in csv.reader(sort_file):
-
                         for i in Product_name:
                             try:
                                 index_Product_name = row.index(i)
@@ -67,6 +66,36 @@ class PriceMachine():
         list_result.sort(key=lambda x: x[1])
         return list_result
 
+    def find_text(self):
+        '''
+        Нолучает текст и возвращает список позиций,
+        содержащий этот текст в названии продукта.
+        <td 1></td><td Филе пангасиуса б/ш ></td><td 92></td><td 1></td><td price_5.csv></td>
+        <td 2></td><td Филе пангасиуса б/ш ></td><td 103></td><td 1></td><td price_4.csv></td>
+        <td 3></td><td Ряпушка вял н/р></td><td 119></td><td 1></td><td price_3.csv></td>
+        <td 4></td><td Килька п/п ></td><td 130></td><td 1></td><td price_0.csv></td>
+        '''
+        file = open(f"{file_html}", "a", encoding='utf-8')
+        b = Airium(source_minify=True)
+        y = 0
+        for i in list_result:
+            if search in i[0]:
+                y += 1
+                b.break_source_line()
+                with b.tr():
+                    with b.td(_t=y):
+                        pass
+                    with b.td(_t=i[0]):
+                        pass
+                    with b.td(_t=i[1]):
+                        pass
+                    with b.td(_t=i[2]):
+                        pass
+                    with b.td(_t=i[3]):
+                        pass
+        file.write(str(b))
+        return b
+
     def export_to_html(self):
         '''
         <!DOCTYPE html>
@@ -87,48 +116,23 @@ class PriceMachine():
         '''
         file = open(f"{file_html}", "w", encoding = 'utf-8')
         a = Airium()
-        # Главное преимущество Airium – DOCTYPE уже включен!
         a('<!DOCTYPE html>')
         with ((((a.html())))):
             a.head()
             a.title(_t="Позиции продуктов")
-            with a.body():
-                with a.table():   # _t="Добро пожаловать на мою страницу!", id="intro"
-                    # a = Airium(source_minify=False, source_line_break_character="\n",)
-                    with a.tr():
-                        a.th(_t="№")
-                        a.th(_t="Наименование")
-                        a.th(_t="Цена")
-                        a.th(_t="Масса")
-                        a.th(_t="Файл")
-                        pass
+        with a.body():
+            with a.table():
+                with a.tr():
+                    a.th(_t="№")
+                    a.th(_t="Наименование")
+                    a.th(_t="Цена")
+                    a.th(_t="Масса")
+                    a.th(_t="Файл")
+                with a.tr(_t=self.find_text()):
+                    pass
             file.write(str(a))
-
-    def find_text():
-        '''
-        Нолучает текст и возвращает список позиций,
-        содержащий этот текст в названии продукта.
-        <td 1></td><td Филе пангасиуса б/ш ></td><td 92></td><td 1></td><td price_5.csv></td>
-        <td 2></td><td Филе пангасиуса б/ш ></td><td 103></td><td 1></td><td price_4.csv></td>
-        <td 3></td><td Ряпушка вял н/р></td><td 119></td><td 1></td><td price_3.csv></td>
-        <td 4></td><td Килька п/п ></td><td 130></td><td 1></td><td price_0.csv></td>
-        '''
-        file = open(f"{file_html}", "a", encoding='utf-8')
-        b = Airium(source_minify=True)
-        with b.tr():
-            y = 0
-            for i in list_result:
-                y += 1
-                b.break_source_line()
-                b.td(y)
-                b.td(i[0])
-                b.td(i[1])
-                b.td(i[2])
-                b.td(i[3])
-            pass
-        file.write(str(b))
 
 if __name__ == "__main__":
     PriceMachine.load_prices(file_path)
-    PriceMachine.export_to_html(file_html)
-    PriceMachine.find_text()
+    PriceMachine().export_to_html()
+
